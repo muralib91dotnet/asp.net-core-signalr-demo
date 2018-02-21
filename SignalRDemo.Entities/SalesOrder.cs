d807b4ef9192c26b21e692451d7ba68136219f97
kartamulia <kartamulia@outlook.com>
2018-02-21 13:26:46 +0700

    Create entities project.

---
+++ SignalRDemo.Entities/SalesOrder.cs
@@ -0,0 +1,33 @@
+﻿namespace SignalRDemo.Entities
+{
+    public class SalesOrder
+    {
+        #region ctor
+
+        public SalesOrder()
+            : base()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int Id { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public SalesOrderState States { get; set; }
+
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+
+        #region Events
+
+        #endregion
+    }
+}
---
+++ SignalRDemo.Entities/SalesOrderState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SignalRDemo.Entities
+{
+    [Flags()]
+    public enum SalesOrderState
+    {
+        Created = 1,
+
+        Delivered = 2,
+
+        Billed = 4,
+
+        Paid = 8,
+
+        Canceled = 16
+
+    }
+}
