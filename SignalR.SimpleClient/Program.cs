8700d092acfb9a99e732a711bed7b76c68bb491a
Murali B <muralib91@hotmail.com>
2020-11-25 17:22:51 +0530

    SignalR Simple client added

---
+++ SignalR.SimpleClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR.SimpleClient
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            HubConnection connection;
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/eventHub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.On<string>("SendNoticeEventToClient", (message) =>
+            {
+                Console.WriteLine($"SignalR event received. Event details: {message}");
+            });
+
+            await connection.StartAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
